a5ca6c69af9daa201b8c7c387b408365e0a97076
Simon Tonner <simon.tonner@world-direct.com>
2021-02-21 23:01:19 +0100

    New sec size comp

--- src/algorithm/execution.py
+++ src/algorithm/execution.py
@@ -108,11 +108,11 @@ def load_circuit_and_benchmark(circuit_synthesis, size, sec_size, sample, path):
     return size, sample, sec_size, num_gates, process_time, initial_rss, final_rss
 
 
-def load_matrix_and_benchmark(circuit_synthesis, input_dir, input_file_name, sec_size):
+def load_matrix_and_benchmark(circuit_synthesis, size, sec_size, sample, path):
 
-    print(f'Loading matrix from file {input_file_name} ...')
+    print(f'Loading matrix from {path} ...')
 
-    mat, size, sample = file_to_matrix(input_dir, input_file_name)
+    mat, size, sample = file_to_matrix(size, path)
 
     print(f'Benchmarking sample {sample} ... matrix size: {size}, section size: {sec_size}')
     _, circuit, process_time, initial_rss, final_rss = check_and_benchmark(circuit_synthesis, mat, sec_size)
