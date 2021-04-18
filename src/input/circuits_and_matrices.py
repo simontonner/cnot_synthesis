7ae8aba71f4923b8e7ff3d411e20a8217000d48a
Simon Tonner <simon.tonner@world-direct.com>
2021-04-18 19:04:08 +0200

    NeuNeu

--- src/input/circuits_and_matrices.py
+++ src/input/circuits_and_matrices.py
@@ -7,8 +7,8 @@ def rand_circuit(size, num_gates):
     circuit = []
 
     for gate_idx in range(num_gates):
-        [target_qubit_idx, control_qubit_idx] = rn.sample(range(size), 2)
-        circuit.append((target_qubit_idx, control_qubit_idx))
+        [control_qubit_idx, target_qubit_idx] = rn.sample(range(size), 2)
+        circuit.append((control_qubit_idx, target_qubit_idx))
 
     return circuit
 
@@ -24,13 +24,37 @@ def identity(size):
 
     return identity
 
-
 def circuit_to_matrix(circuit, size):
 
     matrix = identity(size)
 
-    for (target_row_idx, add_row_idx) in circuit:
+    for (control_row_idx, target_row_idx) in circuit:
         for col_idx in range(size):
-            matrix[target_row_idx][col_idx] ^= matrix[add_row_idx][col_idx]
+            matrix[target_row_idx][col_idx] ^= matrix[control_row_idx][col_idx]
 
     return matrix
+
+
+#import numpy as np
+#def circuit_to_matrix(circuit, size):
+
+  #  matrix = identity(size)
+
+   # for (control_row_idx, target_row_idx) in circuit:
+
+   #     print(np.matrix(matrix))
+
+    #    for col_idx in range(size):
+     #       matrix[target_row_idx][col_idx] ^= matrix[control_row_idx][col_idx]
+
+   # print(np.matrix(matrix))
+
+   # return matrix
+
+
+
+#circuit = [(1, 3), (2, 1), (0, 3), (2, 3), (1, 2), (1, 3), (3, 1), (0, 3), (2, 3), (3, 2), (3, 2), (2, 0), (0, 1), (3, 1), (0, 3), (3, 1)]
+
+
+#circuit_to_matrix(circuit,4)
+
