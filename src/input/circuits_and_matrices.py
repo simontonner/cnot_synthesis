34483ac1e9ecadcb1d8444ad8c09f26cccc8d3e4
Simon Tonner <simon.tonner@world-direct.com>
2021-05-15 21:58:35 +0200

    Migrated remaining notebooks from colab

--- src/input/circuits_and_matrices.py
+++ src/input/circuits_and_matrices.py
@@ -33,28 +33,3 @@ def circuit_to_matrix(circuit, size):
             matrix[target_row_idx][col_idx] ^= matrix[control_row_idx][col_idx]
 
     return matrix
-
-
-#import numpy as np
-#def circuit_to_matrix(circuit, size):
-
-  #  matrix = identity(size)
-
-   # for (control_row_idx, target_row_idx) in circuit:
-
-   #     print(np.matrix(matrix))
-
-    #    for col_idx in range(size):
-     #       matrix[target_row_idx][col_idx] ^= matrix[control_row_idx][col_idx]
-
-   # print(np.matrix(matrix))
-
-   # return matrix
-
-
-
-#circuit = [(1, 3), (2, 1), (0, 3), (2, 3), (1, 2), (1, 3), (3, 1), (0, 3), (2, 3), (3, 2), (3, 2), (2, 0), (0, 1), (3, 1), (0, 3), (3, 1)]
-
-
-#circuit_to_matrix(circuit,4)
-
